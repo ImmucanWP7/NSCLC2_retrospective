3333e4682242230a7457fb56d87a1860b6132a1d
SchulzDan <ubuntu@cloud.science-it.uzh.ch>
2026-07-16 11:13:53 +0000

    lollipop function added

---
+++ revised_figures/Analysis/code/lollipop_function.R
@@ -0,0 +1,63 @@
+# lollipop function
+
+lollipop_plot <- function(data, condition, var_A, var_B, order = TRUE, order_vec, col_A, col_B) {
+  condition_sym <- rlang::sym(condition)
+
+  volcano_df <- data %>%
+    filter(!is.na(!!condition_sym)) %>%
+    group_by(name) %>%
+    mutate(value_z = as.numeric(scale(value))) %>%  # z-score within each signature
+    summarise(
+      delta_z = mean(value_z[!!condition_sym == var_A], na.rm = TRUE) -
+        mean(value_z[!!condition_sym == var_B], na.rm = TRUE),
+      pvalue  = t.test(value_z[!!condition_sym == var_A],
+                       value_z[!!condition_sym == var_B])$p.value,
+      .groups = "drop"
+    ) %>%
+    mutate(
+      padj        = p.adjust(pvalue, method = "BH"),
+      neg_log10_p = -log10(pvalue),
+      direction   = ifelse(delta_z > 0, paste("Up in", var_A), paste("Up in", var_B)),
+      sig         = ifelse(padj < 0.1, TRUE, FALSE)
+    ) %>%
+    arrange(delta_z)
+
+  if(order == TRUE){
+    volcano_df <- volcano_df %>%
+      mutate(name = factor(name, levels = name[order(delta_z)]))  # order for plotting
+    print(volcano_df$name)
+  } else {
+    volcano_df <- volcano_df %>%
+      mutate(name = factor(name, levels = order_vec))  # order for plotting
+  }
+
+
+  dir_A <- paste("Up in", var_A)
+  dir_B <- paste("Up in", var_B)
+
+  p2 <- ggplot(volcano_df, aes(x = delta_z, y = name)) +
+    geom_segment(aes(x = 0, xend = delta_z, y = name, yend = name), color = "grey70") +
+    geom_point(aes(color = direction, size = -log10(pvalue))) +
+    geom_point(
+      data = subset(volcano_df, sig == TRUE),
+      aes(x = delta_z, y = name, size = -log10(pvalue), shape = "FDR < 0.1"),
+      color = "black", fill = NA, stroke = 1
+    ) +
+    scale_color_manual(values = setNames(c(col_A, col_B), c(dir_A, dir_B))) +
+    scale_shape_manual(name = NULL, values = c("FDR < 0.1" = 21)) +
+    geom_vline(xintercept = 0, linetype = "dashed", color = "grey40") +
+    labs(x = paste0("Δ z-score (", var_A, " - ", var_B, ")"), y = NULL,
+         color = "Direction", size = "-log10(p-value)") +
+    theme_minimal() +
+    theme(
+      axis.title.x = element_text(size = 14),
+      axis.text.x  = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 14),
+      axis.text.y  = element_text(size = 14),
+      legend.margin  = margin(t = 0),
+      legend.spacing = unit(0.2, "cm"),
+      legend.text = element_text(size = 12)
+    ) +
+    ggtitle("Imaging modules")
+
+  return(list(data = volcano_df, plot = p2))
+}
