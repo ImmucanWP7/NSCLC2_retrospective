fbb6f70788fc2c5bbbce3437cabc26963e4ca03d
SchulzDan <ubuntu@cloud.science-it.uzh.ch>
2026-07-20 20:01:29 +0000

    updated lollipop function for p value and fdr

--- revised_figures/Analysis/code/lollipop_function.R
+++ revised_figures/Analysis/code/lollipop_function.R
@@ -2,7 +2,6 @@
 
 lollipop_plot <- function(data, condition, var_A, var_B, order = TRUE, order_vec, col_A, col_B) {
   condition_sym <- rlang::sym(condition)
-
   volcano_df <- data %>%
     filter(!is.na(!!condition_sym)) %>%
     group_by(name) %>%
@@ -18,19 +17,18 @@ lollipop_plot <- function(data, condition, var_A, var_B, order = TRUE, order_vec
       padj        = p.adjust(pvalue, method = "BH"),
       neg_log10_p = -log10(pvalue),
       direction   = ifelse(delta_z > 0, paste("Up in", var_A), paste("Up in", var_B)),
-      sig         = ifelse(padj < 0.1, TRUE, FALSE)
+      sig         = ifelse(padj < 0.1, "FDR < 0.1", ifelse(pvalue < 0.05, "P < 0.05", "Non"))
     ) %>%
     arrange(delta_z)
 
-  if(order == TRUE){
+  if (order == TRUE) {
     volcano_df <- volcano_df %>%
-      mutate(name = factor(name, levels = name[order(delta_z)]))  # order for plotting
+      mutate(name = factor(name, levels = name[order(delta_z)]))
     print(volcano_df$name)
   } else {
     volcano_df <- volcano_df %>%
-      mutate(name = factor(name, levels = order_vec))  # order for plotting
+      mutate(name = factor(name, levels = order_vec))
   }
-
 
   dir_A <- paste("Up in", var_A)
   dir_B <- paste("Up in", var_B)
@@ -38,16 +36,18 @@ lollipop_plot <- function(data, condition, var_A, var_B, order = TRUE, order_vec
   p2 <- ggplot(volcano_df, aes(x = delta_z, y = name)) +
     geom_segment(aes(x = 0, xend = delta_z, y = name, yend = name), color = "grey70") +
     geom_point(aes(color = direction, size = -log10(pvalue))) +
-    geom_point(
-      data = subset(volcano_df, sig == TRUE),
-      aes(x = delta_z, y = name, size = -log10(pvalue), shape = "FDR < 0.1"),
-      color = "black", fill = NA, stroke = 1
-    ) +
     scale_color_manual(values = setNames(c(col_A, col_B), c(dir_A, dir_B))) +
-    scale_shape_manual(name = NULL, values = c("FDR < 0.1" = 21)) +
+    ggnewscale::new_scale_color() +
+    geom_point(
+      data = subset(volcano_df, sig != "Non"),
+      aes(x = delta_z, y = name, size = -log10(pvalue), color = sig),
+      shape = 21, fill = NA, stroke = 1
+    ) +
+    scale_color_manual(name = "Significance",
+                       values = c("FDR < 0.1" = "black", "P < 0.05" = "grey60")) +
     geom_vline(xintercept = 0, linetype = "dashed", color = "grey40") +
     labs(x = paste0("Δ z-score (", var_A, " - ", var_B, ")"), y = NULL,
-         color = "Direction", size = "-log10(p-value)") +
+         size = "-log10(p-value)") +
     theme_minimal() +
     theme(
       axis.title.x = element_text(size = 14),
